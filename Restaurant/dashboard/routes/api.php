fba01cce36202a9a78cbd35e3cc50e5b15976d11
JoneCoder <jony@innovationit.com.bd>
2020-01-08 01:17:09 +0600

    run api laravel to vuejs

--- Restaurant/dashboard/routes/api.php
+++ Restaurant/dashboard/routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', function (){
+    return array(1, 2, 3, 4, 'Hello Vuejs');
+});
--- Restaurant/front/src/components/Home.vue
+++ Restaurant/front/src/components/Home.vue
@@ -2,6 +2,8 @@
   <div class="">
     <slider></slider>
     <AppContent></AppContent>
+    <button type="button" @click="newFunction" class="btn btn-success">Api Response</button>
+    <h3>{{ value }}</h3>
     <gallery></gallery>
     <support></support>
   </div>
@@ -19,6 +21,19 @@ export default {
     'AppContent': Content,
     'support': Support,
     'gallery': Gallery
+  },
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    newFunction: function () {
+      this.$http.get('http://127.0.0.1:8000/api/test')
+        .then(function (res) {
+          this.value = res.body
+        })
+    }
   }
 }
 </script>
